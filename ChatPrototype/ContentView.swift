279ade7d83e32ca3d8fc3e82810f3b6491929c36
Wanyu Jiang <wanyu19930930@gmail.com>
2024-11-26 14:08:37 +0900

    Practice

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Knock knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius:8))
+            Text("Who's there")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius:8))
         }
         .padding()
     }
